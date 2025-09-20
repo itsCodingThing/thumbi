a58c6a3341bb64606559489d07a941d1379dff38
bhanu pratap singh <bhanuparitapsingh09@gmail.com>
2025-09-20 21:32:03 +0530

    added notes for refrences

--- src/App.tsx
+++ src/App.tsx
@@ -9,69 +9,92 @@ import FullScreenLoader from "./components/full-screen-loader";
 import generateThumbnail from "./lib/generate-thumbnail";
 
 function App() {
-	const [isPending, startTransition] = useTransition();
-	const [path, setPath] = useState({
-		sourceFile: "",
-		destination: "",
-		ffmpegPath: "",
-	});
+  const [isPending, startTransition] = useTransition();
+  const [path, setPath] = useState({
+    sourceFile: "",
+    destination: "",
+    ffmpegPath: "",
+  });
 
-	const selectFile = async () => {
-		const blob = await fileOpen({
-			multiple: false,
-			directory: false,
-		});
+  const selectFile = async () => {
+    const blob = await fileOpen({
+      multiple: false,
+      directory: false,
+    });
 
-		if (blob) {
-			setPath({ ...path, sourceFile: blob });
-		}
-	};
+    if (blob) {
+      setPath({ ...path, sourceFile: blob });
+    }
+  };
 
-	const whereToSave = async () => {
-		const blob = await fileOpen({
-			directory: true,
-			multiple: false,
-			title: "Select folder to save files in",
-		});
+  const whereToSave = async () => {
+    const blob = await fileOpen({
+      directory: true,
+      multiple: false,
+      title: "Select folder to save files in",
+    });
 
-		if (!blob) {
-			throw new Error("select path where to save");
-		}
+    if (!blob) {
+      throw new Error("select path where to save");
+    }
 
-		setPath({ ...path, destination: blob });
-	};
+    setPath({ ...path, destination: blob });
+  };
 
-	const genThumbnail = async () => {
-		startTransition(async () => {
-			await whereToSave();
-			await generateThumbnail(path.sourceFile, path.destination);
-		});
-	};
+  const genThumbnail = async () => {
+    startTransition(async () => {
+      console.log("thumbnail path", path);
+      await generateThumbnail(path.sourceFile, path.destination);
+    });
+  };
 
-	return (
-		<main className="container min-h-svh flex items-center justify-center bg-black">
-			<FullScreenLoader open={isPending} />
-			<Card>
-				<CardContent>
-					<form className="grid gap-4">
-						<div className="flex gap-2">
-							<Input
-								type="text"
-								placeholder="select video file"
-								defaultValue={path.sourceFile}
-							/>
-							<Button type="button" onClick={selectFile}>
-								select file
-							</Button>
-						</div>
-						<Button type="button" onClick={genThumbnail} disabled={isPending}>
-							generate thumbnails
-						</Button>
-					</form>
-				</CardContent>
-			</Card>
-		</main>
-	);
+  return (
+    <main className="container min-h-svh flex items-center justify-center bg-black">
+      <FullScreenLoader open={isPending} />
+      <Card>
+        <CardContent>
+          <form className="grid gap-4">
+            <div className="flex gap-2">
+              <Input
+                type="text"
+                placeholder="select video file"
+                defaultValue={path.sourceFile}
+              />
+              <Button
+                className="cursor-pointer"
+                type="button"
+                onClick={selectFile}
+              >
+                select file
+              </Button>
+            </div>
+            <div className="flex gap-2">
+              <Input
+                type="text"
+                placeholder="select where to store"
+                defaultValue={path.destination}
+              />
+              <Button
+                className="cursor-pointer"
+                type="button"
+                onClick={whereToSave}
+              >
+                select save
+              </Button>
+            </div>
+            <Button
+              className="cursor-pointer"
+              type="button"
+              onClick={genThumbnail}
+              disabled={isPending}
+            >
+              generate thumbnails
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </main>
+  );
 }
 
 export default App;
