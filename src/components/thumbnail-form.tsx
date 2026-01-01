483dfa7e54874060ba925becf10eb6e116062af5
bhanu pratap singh <bhanuparitapsingh09@gmai.com>
2026-01-01 20:14:42 +0530

    fix import

--- src/components/thumbnail-form.tsx
+++ src/components/thumbnail-form.tsx
@@ -3,61 +3,61 @@ import { open as fileOpen } from "@tauri-apps/plugin-dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent } from "@/components/ui/card";
-import { generateThumbnails } from "@/lib/ffmpeg";
+import { ffmpeg } from "@/lib/ffmpeg";
 import { useFullScreenLoader } from "@/components/full-screen-loader";
 import { useSourceFilePath } from "@/components/source-file";
 
 export default function ThumbnailForm() {
-  const { setScreenLoader } = useFullScreenLoader();
-  const [sourcePath] = useSourceFilePath();
-  const [path, setPath] = useState("");
+	const { setScreenLoader } = useFullScreenLoader();
+	const [sourcePath] = useSourceFilePath();
+	const [path, setPath] = useState("");
 
-  const whereToSave = async () => {
-    const blob = await fileOpen({
-      directory: true,
-      multiple: false,
-      title: "Select folder to save files in",
-    });
+	const whereToSave = async () => {
+		const blob = await fileOpen({
+			directory: true,
+			multiple: false,
+			title: "Select folder to save files in",
+		});
 
-    if (!blob) {
-      throw new Error("select path where to save");
-    }
+		if (!blob) {
+			throw new Error("select path where to save");
+		}
 
-    setPath(blob);
-  };
+		setPath(blob);
+	};
 
-  const genThumbnail = async () => {
-    setScreenLoader(true);
-    await generateThumbnails(sourcePath, path);
-    setScreenLoader(false);
-  };
+	const genThumbnail = async () => {
+		setScreenLoader(true);
+		await ffmpeg.generateThumbnails(sourcePath, path);
+		setScreenLoader(false);
+	};
 
-  return (
-    <Card>
-      <CardContent>
-        <div className="grid grid-cols-2 gap-2">
-          <Input
-            disabled
-            type="text"
-            placeholder="select where to store"
-            defaultValue={path}
-          />
-          <Button
-            className="cursor-pointer"
-            type="button"
-            onClick={whereToSave}
-          >
-            select save
-          </Button>
-          <Button
-            className="cursor-pointer col-span-2"
-            type="button"
-            onClick={genThumbnail}
-          >
-            generate thumbnails
-          </Button>
-        </div>
-      </CardContent>
-    </Card>
-  );
+	return (
+		<Card>
+			<CardContent>
+				<div className="grid grid-cols-2 gap-2">
+					<Input
+						disabled
+						type="text"
+						placeholder="select where to store"
+						defaultValue={path}
+					/>
+					<Button
+						className="cursor-pointer"
+						type="button"
+						onClick={whereToSave}
+					>
+						select save
+					</Button>
+					<Button
+						className="cursor-pointer col-span-2"
+						type="button"
+						onClick={genThumbnail}
+					>
+						generate thumbnails
+					</Button>
+				</div>
+			</CardContent>
+		</Card>
+	);
 }
